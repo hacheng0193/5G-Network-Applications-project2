eff9650a6f0e5440bad1b75eff9ba055a2100ad2
hacheng0193 <alex.chenghengan@gmail.com>
2026-04-22 16:17:43 +0800

    part 1: implement subscription management in CustomizedRMS

--- amf/internal/rms/rms.go
+++ amf/internal/rms/rms.go
@@ -4,14 +4,57 @@ import "github.com/free5gc/util/fsm"
 
 type CustomizedRMS struct {
 	// implement your customized RMS fields here
+	subscriptions map[string]Subscription // key: subId
 }
 
 func NewRMS(
 // implement your customized RMS initialization here
 ) fsm.RMS {
-	return &CustomizedRMS{}
+	return &CustomizedRMS{
+		make(map[string]Subscription),
+	}
 }
 
+func (rms *CustomizedRMS) Add(sub Subscription) {
+	// implement your customized RMS add logic here
+	rms.subscriptions[subscription.SubID] = sub
+}
+
+func (rms *CustomizedRMS) Modify(subid string, sub Subscription) bool {
+	// implement your customized RMS modify logic here
+	if _, exists := rms.subscriptions[subid]; exists {
+		rms.subscriptions[subid] = sub
+		return true
+	}
+	return false
+}
+
+func (rms *CustomizedRMS) Query(subid string) (Subscription, bool) {
+	// implement your customized RMS query logic here
+	sub, exists := rms.subscriptions[subid]
+	return sub, exists
+}
+
+func (rms *CustomizedRMS) QueryAll() []Subscription {
+	// implement your customized RMS query all logic here
+	subs := make([]Subscription, 0, len(rms.subscriptions))
+	for _, sub := range rms.subscriptions {
+		subs = append(subs, sub)
+	}
+	return subs
+}
+
+
+
+func (rms *CustomizedRMS) Delete(subid string) bool{
+	// implement your customized RMS remove logic here
+	if _, exists := rms.subscriptions[subid]; exists {
+		delete(rms.subscriptions, subid)
+		return true
+	}
+	return false
+}
 func (rms *CustomizedRMS) HandleEvent(state *fsm.State, event fsm.EventType, args fsm.ArgsType, trans fsm.Transition) {
 	// implement your customized RMS logic here
+	
 }
--- amf/internal/sbi/api_rms.go
+++ amf/internal/sbi/api_rms.go
@@ -17,5 +17,71 @@ func (s *Server) getRMSRoutes() []Route {
 			},
 		},
 		// add more Route based on provided spec
+		// Get operation
+		// Return the currently existing UE-RM subscriptions
+		{
+			Name:    "getSubscription", 
+			Method:  http.MethodGet,
+			Pattern: "/subscriptions",
+			APIFunc: func(c *gin.Context) {
+				subs := s.rms.QueryAll()
+				c.JSON(http.StatusOK, subs)
+			}
+		},
+		// Post operation
+		// Create a new UE-RM subscription
+		// The SubId in the POST request can be filled arbitrarily (e.g., sub-001). Your API handler is then responsible for populating the assigned SubId and returning it.
+		{
+			Name:    "postSubscription", 
+			Method:  http.MethodPost,
+			Pattern: "/subscriptions",
+			APIFunc: func(c *gin.Context) {
+				var sub Subscription
+				if err := c.ShouldBindJSON(&sub); err != nil {
+					c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+					return
+				}
+				s.rms.Add(sub)
+				c.JSON(http.StatusCreated, subscription.SubID)
+
+			}
+		},
+		// PUT Operation
+		// Create or overwrite a UE-RM subscription
+		// The relevant schemas are the same as those for the POST operation.
+		{
+			Name:    "putSubscription",
+			Method:  http.MethodPut,
+			Pattern: "/subscriptions/:subId",
+			APIFunc: func(c *gin.Context) {
+				subId := c.Param("subId")
+				var sub Subscription
+				if err := c.ShouldBindJSON(&sub); err != nil {
+					c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+					return
+				}
+				if s.rms.Modify(subId, sub) {
+					c.JSON(http.StatusOK, gin.H{"message": "Subscription modified"})
+				} else {
+					s.rms.Add(sub)
+					c.JSON(http.StatusCreated, gin.H{"message": "Subscription created"})
+				}
+			}
+		},
+		// Delete operation
+		// Delete an existing UE-RM subscription.
+		{
+			Name:    "deleteSubscription",
+			Method:  http.MethodDelete,
+			Pattern: "/subscriptions/:subId",
+			APIFunc: func(c *gin.Context) {
+				subId := c.Param("subId")
+				if s.rms.Delete(subId) {
+					c.JSON(http.StatusOK, gin.H{"message": "Subscription deleted"})
+				} else {
+					c.JSON(http.StatusNotFound, gin.H{"error": "Subscription not found"})
+				}
+			}
+		}
 	}
 }
